c1afa9ba843359d9e6e0ba43f37798d125b4c68e
Luke Geneva <lukegeneva@gmail.com>
2018-04-03 15:28:09 -0500

    Install Vundle and plugins

--- install.sh
+++ install.sh
@@ -1,3 +1,5 @@
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+
 ln -sv ~/dotfiles/runcom/.bash_profile ~/.bash_profile
 ln -sv ~/dotfiles/runcom/.inputrc ~/.inputrc
 ln -sv ~/dotfiles/git/.gitconfig ~/.gitconfig
